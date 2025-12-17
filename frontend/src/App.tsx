80ae0404c310da78951076930a6e90eebda7e551
Khushi Joshi <132840479+khushinj@users.noreply.github.com>
2025-12-17 13:03:05 +0000

    Project Initiation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <p className='font-bold text-red-700 text-2xl'>hi everyone</p>
+    </>
+  )
+}
+
+export default App
