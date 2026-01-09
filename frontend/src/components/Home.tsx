eb367485568af673ad673f9cac8ccf162f40de91
Khushi Joshi <132840479+khushinj@users.noreply.github.com>
2026-01-09 08:52:06 +0000

    Added 1 card and plus sign border along with responsiveness

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import card1 from '../assets/Card1.png';
 import card2 from '../assets/Card 3.png';
 import card3 from '../assets/Card 5.png';
+import card4 from '../assets/Card 6.png';
 import plus from '../assets/plus.png';
 import Form from './Form';
 import { useState } from 'react';
@@ -14,6 +15,7 @@ function Home() {
     { id: 1, image: card1 },
     { id: 2, image: card2 },
     { id: 3, image: card3 },
+    { id: 4, image: card4 },
   ];
 
   const handleCardClick = (cardId: number) => {
@@ -24,8 +26,8 @@ function Home() {
     <>
       <div className='grid grid-flow-col gap-10 justify-between'>
         <p className='agbalumo-regular font-bold text-5xl'>Juno</p>
-        <button 
-          className='bg-gray-300 w-15 h-15 rounded-full flex items-center justify-center p-2' 
+        <button
+          className='bg-gray-300 border border-black border-2 w-15 h-15 rounded-full flex items-center justify-center p-2'
           onClick={() => setShowForm(true)}
         >
           <img src={plus} alt="" className='h-5 w-5' />
@@ -33,14 +35,14 @@ function Home() {
       </div>
       {showForm && <Form onClose={() => setShowForm(false)} />}
       <div className='card-section'>
-        <div className='mt-20 grid-row-4 grid-flow-col grid gap-7'>
+        <div className='mt-20 lg:grid-cols-3 md:grid-cols-2 sm:justify-items-center sm:grid-cols-1 grid gap-7'>
           {cardData.map(card => (
-            <img 
-              key={card.id} 
-              src={card.image} 
-              alt={`card ${card.id}`} 
-              className='h-120 shadow-xl rounded-4xl cursor-pointer hover:opacity-90 transition-opacity' 
-              onClick={() => handleCardClick(card.id)} 
+            <img
+              key={card.id}
+              src={card.image}
+              alt={`card ${card.id}`}
+              className='h-120 shadow-xl rounded-4xl cursor-pointer hover:opacity-90 transition-opacity'
+              onClick={() => handleCardClick(card.id)}
             />
           ))}
         </div>
